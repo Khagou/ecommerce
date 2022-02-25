50882e69ffd40fc560e3e3fb692ed34bf488d767
Maxence <max.verdom@gmail.com>
2022-02-25 12:01:54 +0100

    'début-tp'

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>e-commerce</title>
+</head>
+<body>
+    <nav class="grid grid-cols-10">
+        <a href="/">Ecommerce</a>
+        <ul>
+            <li>Accueil</li>
+            <li>Produits</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</body>
+</html>
