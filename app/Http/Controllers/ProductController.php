410aa22dd8cc09ef433aaba2ad14929928c82f75
Maxence <max.verdom@gmail.com>
2022-02-27 20:24:25 +0100

    'ProductController'

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view ('product.index',[
+            'products' => Product::latest('created_at')->paginate(6)
+        ]);
+    }
+
+    public function show(Product $product)
+    {
+        return view ('product.show',[
+            'product' => $product
+        ]);
+    }
 }
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,14 +7,22 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>e-commerce</title>
 </head>
-<body>
-    <nav class="grid grid-cols-10">
-        <a href="/">Ecommerce</a>
-        <ul>
-            <li>Accueil</li>
-            <li>Produits</li>
-            <li>Contact</li>
+<body class="">
+    <nav class="">
+       <div class="mx=auto ">
+        <ul class="grid grid-cols-10">
+            <li><a href="/" class="col-start-2">Ecommerce</a></li>
+            <li><a href="/" class="col-start-3">Accueil</a></li>
+            <li><a href="/produits" class="col-start-4">Produits</a></li>
+            <li><a href="/contact" class="col-start-5">Contact</a></li>
         </ul>
+
+        <form action="" method="post">
+            <input type="text" name="search" id="" placeholder="Rechercher">
+            <button type="button"></button>
+        </form>
+        <a href="/"></a>
+       </div>
     </nav>
 </body>
 </html>
